b412b38fe3a2512a339c735184189d3949c58b7d
80auline <p.beurier@gmail.com>
2023-06-05 11:59:41 +0400

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -8,3 +8,8 @@ ls -al
 echo '-----> Creating ssh key: secrets.RTEST in base64'
 mkdir ~/.ssh
 echo "$RTEST" | base64 --decode -i > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519 # permissioning
+eval "$(ssh-agent -s)" # setting ssh environment variable
+
+echo '-----> Adding keys to ssh-agent'
+ssh-add ~/.ssh/id_ed25519
